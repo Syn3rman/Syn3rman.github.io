a2f0114089175bcdd2a410d7c39c61ec6fb48c5f
Aditya <thesyn3rman@gmail.com>
2025-04-04 21:29:22 -0700

    update experience
    Signed-off-by: Aditya <thesyn3rman@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,45 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
+const { src, dest, watch, series } = require('gulp');
+const plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
-gulp.task('scripts', function() {
-    return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
-        }))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('./js'));
-});
+function scripts() {
+  return src('./js/scripts.js')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(babel({ presets: [['@babel/env', { modules: false }]] }))
+    .pipe(uglify({ output: { comments: '/^!/' } }))
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest('./js'))
+    .pipe(browserSync.stream());
+}
 
-gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
-        .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-});
+function styles() {
+  return src('./scss/styles.scss')
+    .pipe(wait(250))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(dest('./css'))
+    .pipe(browserSync.stream());
+}
 
-gulp.task('watch', function() {
-    gulp.watch('./js/scripts.js', gulp.series('scripts'));
-    gulp.watch('./scss/styles.scss', gulp.series('styles'));
-});
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  watch('./scss/styles.scss', styles);
+  watch('./js/scripts.js', scripts);
+  watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.default = series(styles, scripts, serve);
